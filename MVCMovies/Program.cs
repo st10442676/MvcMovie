fb7299596015f0b9c4ee96f7ade41587d8849988
st10442676 <tiashapillay0407@gmail.com>
2025-02-25 13:36:00 +0200

    Add project files.

---
+++ MVCMovies/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Models;
+using MVCMovies.Data;
+using MVCMovies.Models;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MVCMoviesContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MVCMoviesContext") ?? throw new InvalidOperationException("Connection string 'MVCMoviesContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
